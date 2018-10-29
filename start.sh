5829c695426d8c1ae88e9e91dab6c03d7bff5b19
Oleksandr <ext-oleksandr.rudenko@here.com>
2018-10-29 18:12:38 +0200

    edit

--- start.sh
+++ start.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 #before use, please run "sudo -s" and input admin password "chmod 775 start.sh", after that run exit, and run "./start.sh"
+echo "before use, please run sudo -s and input admin password chmod 775 start.sh, after that run exit, and run start.sh"
 sudo apt install -y gnupg
 sudo apt install -y gnupg2
 read -p 'input pass: ' pass
+echo "###################"
 read -p 'input git user name: ' gitusnm
+echo "###################"
 read -p 'input git user email: ' gitusem
 gpg --passphrase $pass --output init.sh --decrypt init.sh.gpg
 gpg --passphrase $pass --output repo.sh --decrypt repo.sh.gpg
 gpg --passphrase $pass --output conf.sh --decrypt conf.sh.gpg
+echo "###################"
 echo "This script is about to run another scripts"
+echo "###################"
 chmod 775 init.sh
 chmod 775 repo.sh
 chmod 775 conf.sh
 echo "run init"
 sh ./init.sh
+echo "###################"
 echo "configuration"
+echo "###################"
 export gitusnm
 export gitusem
 sh ./conf.sh
-#echo "run repo"
-#export gitusnm
-#sh ./repo.sh
+echo "###################"
+echo "run repo"
+echo "###################"
+export gitusnm
+sh ./repo.sh
 
