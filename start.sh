94ea79f26bc03a09a44fe06b90bf17ad358333c9
Oleksandr <ext-oleksandr.rudenko@here.com>
2018-12-12 09:57:37 +0200

    add libs

--- start.sh
+++ start.sh
@@ -4,35 +4,35 @@ echo "before use, please run sudo -s and input admin password chmod 775 start.sh
 sudo apt install -y gnupg
 sudo apt install -y gnupg2
 read -p 'input pass: ' pass
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 read -p 'input git user name: ' gitusnm
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 read -p 'input git user email: ' gitusem
 gpg --passphrase $pass --output init.sh --decrypt init.sh.gpg
 gpg --passphrase $pass --output repo.sh --decrypt repo.sh.gpg
 gpg --passphrase $pass --output conf.sh --decrypt conf.sh.gpg
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 echo "\e[1;32m This script is about to run another scripts \e[0m"
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 chmod 775 init.sh
 chmod 775 repo.sh
 chmod 775 conf.sh
 echo "run init"
 sh ./init.sh
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 echo "\e[1;32m configuration \e[0m"
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 export gitusnm
 export gitusem
 sh ./conf.sh
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 echo "\e[1;32m run repo \e[0m"
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 export gitusnm
 sh ./repo.sh
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 echo "\e[1;32m clean \e[0m"
-echo "\e[1;32m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\e[0m"
+echo "\e[1;32m !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \e[0m"
 rm -rf init.sh.gpg
 rm -rf repo.sh.gpg
 rm -rf conf.sh.gpg
